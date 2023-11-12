f1a2c47d5bce421215da05534e5f4d477c7af759
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-11-12 15:23:51 +0530

    works

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+- make a button to open up question in browser
+- multiple prompts for one submission (cus of testing and then accepted, remove those)
+- more descriptive text in popup 
+- build workflow
+- file based handle read
--- clean.ps1
+++ clean.ps1
@@ -2,7 +2,9 @@ rm *.vcxproj
 rm *.vcxproj.filters
 rm *.sln
 rm *.cmake
-$paths = 'CMakeFiles', '_deps', 'CMakeCache.txt'
+rm *.dll
+rm *.exe
+$paths = 'Makefile', 'CMakeFiles', '_deps', 'CMakeCache.txt'
 foreach ($path in $paths) {
     if (Test-Path $path) {
       rm -Force -Recurse $path
